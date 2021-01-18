fbfa743b1c04b55e5ac0ce26d90f1e1de61075e4
Alex Mercea <alex.mercea@lxmrc.com>
2021-01-18 15:18:36 +1100

    Disable unwanted generators

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,18 @@ module Bloggy
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.helper false
+      g.assets false
+
+      g.test_framework :rspec,
+        fixtures:         false,
+        view_specs:       false,
+        helper_specs:     false,
+        routing_specs:    false,
+        request_specs:    false,
+        controller_specs: false
+    end
   end
 end
