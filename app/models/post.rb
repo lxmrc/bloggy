b7693ff9a7149919669ed0a6ddfe08f0ba92ad84
Alex Mercea <alex.mercea@lxmrc.com>
2021-01-23 10:54:14 +1100

    Allow posts without text

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :content, presence: true
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one_attached :image
